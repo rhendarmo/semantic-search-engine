4ba664cd4132918a8d7a6493fed76ac0693be34d
Raditya Hendarmo <hendarmor@gmail.com>
2026-02-02 22:59:13 -0800

    Update CORS to include vercel

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,10 @@ app = FastAPI(title="Movie Semantic Search API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://semantic-search-engine-six.vercel.app/"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
